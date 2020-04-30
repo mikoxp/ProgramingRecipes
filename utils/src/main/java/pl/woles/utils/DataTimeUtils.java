cc4e8466dcb8dbb27a9b0ae81e835907d262f73b
olesmichalxp@tlen.pl <olesmichalxp@tlen.pl>
2020-04-30 20:59:52 +0200

    DataTimeUtlis toMiliseconds

--- utils/src/main/java/pl/woles/utils/DataTimeUtils.java
+++ utils/src/main/java/pl/woles/utils/DataTimeUtils.java
@@ -1,6 +1,8 @@
 package pl.woles.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -8,16 +10,57 @@ import java.time.format.DateTimeFormatter;
  */
 public class DataTimeUtils {
 
+    public final static String FORMAT_WITH_HOURS = "dd-MM-yyyy HH:mm";
+    public final static String FORMAT_ONLY_DATE = "dd-MM-yyyy";
 
     /**
      * Change LocalDateTime to string with format.
      *
      * @param dateTime date with time
-     * @param format   format
+     * @param format format
      * @return date with string
      */
     public static String parseDateTimeToString(LocalDateTime dateTime, String format) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
         return dateTime.format(formatter);
     }
+
+    /**
+     * Change string to LocalDateTime  with format.
+     *
+     * @param dateTime string datetime
+     * @param format format
+     * @return localdatetime
+     */
+    public static LocalDateTime parseStringToLocalDateTime(String dateTime, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return LocalDateTime.parse(dateTime, formatter);
+    }
+
+    /**
+     * @param localDateTime localDateTime
+     * @param actualZone actual zone
+     * @param destinationZone destination zone
+     * @return datetime with zone
+     */
+    public static LocalDateTime setDateToZone(LocalDateTime localDateTime, String actualZone, String destinationZone) {
+        ZoneId actual = ZoneId.of(actualZone);
+        ZoneId destination = ZoneId.of(destinationZone);
+        return setDateToZone(localDateTime, actual, destination);
+    }
+
+    /**
+     * @param localDateTime localDateTime
+     * @param actualZone actual zone
+     * @param destinationZone destination zone
+     * @return datetime with zone
+     */
+    public static LocalDateTime setDateToZone(LocalDateTime localDateTime, ZoneId actualZone, ZoneId destinationZone) {
+        return localDateTime.atZone(actualZone).withZoneSameInstant(destinationZone).toLocalDateTime();
+    }
+
+    public static long toMiliseconds(ZonedDateTime zonedDateTime) {
+        return zonedDateTime.toInstant().toEpochMilli();
+    }
+
 }
--- utils/src/test/java/pl/woles/utils/DataTimeUtilsTest.java
+++ utils/src/test/java/pl/woles/utils/DataTimeUtilsTest.java
@@ -1,25 +1,26 @@
 package pl.woles.utils;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.LocalDateTime;
-
-import static org.junit.Assert.*;
-
 public class DataTimeUtilsTest {
 
-    private String format_1;
-    private LocalDateTime localDateTime;
-    private String stringDateTime;
+    private final static String FORMAT_WITH_MINUTES = "dd-MM-yyyy HH:mm";
+    private final static String TIMEZONE_UTC = "UTC";
+    private final static String TIMEZONE_PLUS_4 = "Etc/GMT-4";
+    private LocalDateTime localDateTime_1;
+    private String stringDateTime_1;
 
     @Before
     public void setUp() {
-        format_1="dd-MM-yyyy HH:mm";
-        localDateTime = LocalDateTime.of(2019, 1, 1, 1, 1);
-        stringDateTime="01-01-2019 01:01";
+        localDateTime_1 = LocalDateTime.of(2019, 1, 1, 1, 1);
+        stringDateTime_1 = "01-01-2019 01:01";
     }
 
     @After
@@ -27,13 +28,44 @@ public class DataTimeUtilsTest {
     }
 
     @Test
-    public void parseDateTimeToString_correctFormat_string(){
-        String result = DataTimeUtils.parseDateTimeToString(localDateTime, format_1);
-        Assert.assertEquals(stringDateTime,result);
+    public void parseDateTimeToString_correctFormat_string() {
+        String result = DataTimeUtils.parseDateTimeToString(localDateTime_1, FORMAT_WITH_MINUTES);
+        Assert.assertEquals(stringDateTime_1, result);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void parseDateTimeToString_noCorrectFormat_string(){
-        DataTimeUtils.parseDateTimeToString(localDateTime, "abc");
+    public void parseDateTimeToString_noCorrectFormat_string() {
+        DataTimeUtils.parseDateTimeToString(localDateTime_1, "abc");
+    }
+
+    @Test
+    public void parseStringToLocalDateTime_correctData() {
+        final LocalDateTime result = DataTimeUtils.parseStringToLocalDateTime(stringDateTime_1, FORMAT_WITH_MINUTES);
+        Assert.assertEquals(localDateTime_1, result);
+    }
+
+    @Test
+    public void setDateToZone_changeDate_zoneIdVersion() {
+        ZoneId actual = ZoneId.of(TIMEZONE_PLUS_4);
+        ZoneId destination = ZoneId.of(TIMEZONE_UTC);
+        final LocalDateTime expected = localDateTime_1.minusHours(4);
+        final LocalDateTime result = DataTimeUtils.setDateToZone(localDateTime_1, actual, destination);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void setDateToZone_changeDate_stringVersion() {
+        final LocalDateTime expected = localDateTime_1.plusHours(4);
+        final LocalDateTime result = DataTimeUtils.setDateToZone(localDateTime_1, TIMEZONE_UTC, TIMEZONE_PLUS_4);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void toMiliseconds(){
+        final LocalDateTime now = LocalDateTime.now();
+        final ZonedDateTime zonedDateTime = now.atZone(ZoneOffset.UTC);
+        final long expected = zonedDateTime.toInstant().toEpochMilli();
+        final long result = DataTimeUtils.toMiliseconds(zonedDateTime);
+        Assert.assertEquals(expected, result);
     }
 }
